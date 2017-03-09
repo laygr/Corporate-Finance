3cabe4eba9b732625060ae9b5b8e7390dfecf1f2
laygr <laygr@outlook.com>
2017-03-09 16:01:33 -0600

    VS Project

---
+++ Corporate Finance.Tests/Tests.fs
@@ -0,0 +1,58 @@
+﻿namespace CorporateFinance.Tests
+
+module Options =
+
+    open NUnit.Framework
+    open FsUnit
+
+    open CorporateFinance.Options.Binomial
+
+    [<Test>]
+    let abandonment() =
+        let option = Abandonment 190.
+        let currentPrice, multiplierWhenUp, multiplierWhenDown, exerciseValue, riskFreeRate, periods = 200., 1.25, 0.8, 0., 0.05, 2
+        let value = optionalCallValuation currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods [option]
+        value - 212.6984127
+        |> should be (lessThan 0.0001)
+
+    [<Test>]
+    let contraction() =
+        let option = Contraction (0.2, 50.)
+        let currentPrice, multiplierWhenUp, multiplierWhenDown, exerciseValue, riskFreeRate, periods = 200., 1.25, 0.8, 0., 0.05, 2
+        let value = optionalCallValuation currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods [option]
+        value - 209.8586266
+        |> should be (lessThan 0.0001)
+
+    [<Test>]
+    let expansion() =
+        let option = Expansion (0.3, 70.)
+        let currentPrice, multiplierWhenUp, multiplierWhenDown, exerciseValue, riskFreeRate, periods = 200., 1.25, 0.8, 0., 0.05, 2
+        let value = optionalCallValuation currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods [option]
+        value - 206.64875
+        |> should be (lessThan 0.0001)
+
+    [<Test>]
+    let ``multiples opciones``() =
+        let abandonment = Abandonment 190.
+        let contraction = Contraction (0.2, 50.)
+        let expansion = Expansion (0.3, 70.)
+        let options = [abandonment; contraction; expansion]
+        let currentPrice, multiplierWhenUp, multiplierWhenDown, exerciseValue, riskFreeRate, periods = 200., 1.25, 0.8, 0., 0.05, 2
+        let value = optionalCallValuation currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods options
+        value - 222.2362196
+        |> should be (lessThan 0.0001)
+    
+
+    open CorporateFinance.Options
+    [<Test>]
+    let ``ejercicio black and scholes``() =
+        let wacc = 0.12
+        let riskFreeRate = 0.05
+        let stdDev = 0.4
+        let div = 600000.
+        let currentPrice = 6000000.
+        let exerciseValue = 5000000.
+        let time = 1.
+        let v = callValue currentPrice exerciseValue div wacc riskFreeRate stdDev time
+        v - 1208473.5232
+        |> should be (lessThan 0.0001)
---
+++ Corporate Finance/Library1.fs
@@ -0,0 +1,84 @@
+﻿namespace CorporateFinance
+
+module Investment =
+        let npv values rate =
+            values
+            |> List.mapi (fun i v -> v / (rate + 1.) ** (float i))
+            |> List.sum
+
+        let pv value rate time =
+            value / (1. + rate) ** time
+
+module BlackAndScholes =
+    open Investment
+
+    let blackAndScholes currentPrice exerciseValue annualVolatility timeToMaturity riskFreeRate =
+        let pv = pv exerciseValue riskFreeRate timeToMaturity
+
+        let d1 = 
+            log(currentPrice / pv) / (annualVolatility * sqrt(timeToMaturity))
+            + annualVolatility * sqrt(timeToMaturity) / 2.
+
+        let d2 =
+            d1 - annualVolatility * sqrt(timeToMaturity)
+
+        let normEst = new MathNet.Numerics.Distributions.Normal()
+
+        let n1 = normEst.CumulativeDistribution(d1)
+        let n2 = normEst.CumulativeDistribution(d2)
+        currentPrice * n1 - pv * n2
+
+module Options =
+    module Binomial =
+        
+        let replicaPortfolio callWhenUp callWhenDown priceWhenUp priceWhenDown stockPrice riskFreeRate : float=
+            let numberOfStocksToBuy = (callWhenUp - callWhenDown) / (priceWhenUp - priceWhenDown)
+            let shortedRiskFree = (callWhenDown - priceWhenDown * numberOfStocksToBuy) / (1. + riskFreeRate)
+            numberOfStocksToBuy * stockPrice + shortedRiskFree
+
+        type OptionType =
+             | Abandonment of float
+             | Contraction of float * float //reduction percentage, extra flow
+             | Expansion of float * float //expansion percentage, cost
+             | None
+
+        let valueForOption value pv option =
+            match option with
+            | Abandonment abandonment -> max pv abandonment
+            | Contraction (reductionP, extraFlow) -> max pv (value * (1.-reductionP) + extraFlow)
+            | Expansion (expansionP, cost) -> max pv (value * (1. + expansionP) - cost)
+            | None -> value
+
+        let valueForOptions value pv options =
+            List.map (valueForOption value pv) options
+            |> List.max
+
+        let rec optionalCallValuation' currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods remainingPeriods options =
+            if remainingPeriods = 0
+            then
+                let presentValue = (currentPrice - exerciseValue)
+                valueForOptions currentPrice presentValue options
+            else
+
+            let priceWhenUp = currentPrice * multiplierWhenUp
+            let priceWhenDown = currentPrice * multiplierWhenDown
+
+            let alternativePayoffForUp = optionalCallValuation' priceWhenUp multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods (remainingPeriods - 1) options
+            let alternativePayoffForDown = optionalCallValuation' priceWhenDown multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods (remainingPeriods - 1) options
+
+            let callWhenUp = max (priceWhenUp - exerciseValue) alternativePayoffForUp
+            let callWhenDown = max (priceWhenDown - exerciseValue) alternativePayoffForDown
+
+            let presentValue = replicaPortfolio callWhenUp callWhenDown priceWhenUp priceWhenDown currentPrice riskFreeRate
+            if periods = remainingPeriods
+            then presentValue
+            else valueForOptions currentPrice presentValue options
+
+        let optionalCallValuation currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods options =
+            optionalCallValuation' currentPrice multiplierWhenUp multiplierWhenDown exerciseValue riskFreeRate periods periods options
+
+    open BlackAndScholes
+    open Investment
+    let callValue currentPrice exerciseValue div wacc riskFreeRate stdDev time =
+        let actualValue = currentPrice - (pv div wacc time) // currentPrice without dividends
+        blackAndScholes actualValue exerciseValue stdDev time riskFreeRate
